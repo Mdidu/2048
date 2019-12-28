5da33cf3fc6fbdb974ea3351848c4aaecc98564b
Mdidu <alexandre.meddas@gmail.com>
2019-12-28 15:05:04 +0100

    2048 Games nearly complete

--- js/event.js
+++ js/event.js
@@ -20,7 +20,6 @@ $(document).keydown(function(e){
             break;
     }
     postMoove();
-
 });
 function postMoove(){
     if(gameWin === false){
@@ -33,13 +32,9 @@ function postMoove(){
     }
     loose();
 }
-
-var container = $("body");
-
-container.addEventListener("touchstart", startTouch, false);
-// container.touchstart(startTouch, false);
-// container.touchmove(moveTouch, false);
-container.addEventListener("touchmove", moveTouch, false);
+// mobile event
+$(document).on("touchstart",startTouch);
+$(document).on("touchmove",moveTouch);
 
 // Swipe Up / Down / Left / Right
 var initialX = null;
@@ -71,12 +66,10 @@ function moveTouch(e) {
             // swiped left
             fusionHorizontal(3, 2, 1, 0);
             postMoove();
-            alert("swipe left");
         } else {
             // swiped right
             fusionHorizontal(0, 1, 2, 3);
             postMoove();
-            alert("swipe right");
         }
     } else {
         // sliding vertically
@@ -84,12 +77,10 @@ function moveTouch(e) {
             // swiped up
             verticalFusion(3, 2, 1, 0);
             postMoove();
-            alert("swipe up");
         } else {
             // swiped down
             verticalFusion(0, 1, 2, 3);
             postMoove();
-            alert("swipe down");
         }
     }
 
@@ -98,73 +89,3 @@ function moveTouch(e) {
 
     e.preventDefault();
 }
-
-//mobile event
-// $(document).on( "swipedown", function() {
-//     verticalFusion(0, 1, 2, 3);
-//     postMoove();
-//     alert("swipe down");
-// });
-// $(document).on( "swipeup", function() {
-//     verticalFusion(3, 2, 1, 0);
-//     postMoove();
-//     alert("swipe up");
-// } );
-
-// $('#main').swiperight(function() {
-//
-//     fusionHorizontal(0, 1, 2, 3);
-//     postMoove();
-//     alert("swipe right");
-// } );
-//
-// $('#main').swipeleft(function() {
-//
-//     fusionHorizontal(3, 2, 1, 0);
-//     postMoove();
-//     alert("swipe left");
-// } );
-
-// let touchstartX = 0;
-// let touchstartY = 0;
-// let touchendX = 0;
-// let touchendY = 0;
-//
-// const gestureZone = document.getElementById('main');
-//
-// gestureZone.addEventListener('touchstart', function(event) {
-//     touchstartX = event.changedTouches[0].screenX;
-//     touchstartY = event.changedTouches[0].screenY;
-// }, false);
-//
-// gestureZone.addEventListener('touchend', function(event) {
-//     touchendX = event.changedTouches[0].screenX;
-//     touchendY = event.changedTouches[0].screenY;
-//     handleGesture();
-// }, false);
-//
-// function handleGesture() {
-//     if (touchendX <= touchstartX) {
-//         fusionHorizontal(3, 2, 1, 0);
-//         postMoove();
-//     }
-//
-//     if (touchendX >= touchstartX) {
-//         fusionHorizontal(0, 1, 2, 3);
-//         postMoove();
-//     }
-//
-//     if (touchendY <= touchstartY) {
-//         verticalFusion(3, 2, 1, 0);
-//         postMoove();
-//     }
-//
-//     if (touchendY >= touchstartY) {
-//         verticalFusion(0, 1, 2, 3);
-//         postMoove();
-//     }
-//
-//     if (touchendY === touchstartY) {
-//         console.log('Tap');
-//     }
-// }
